2e3f49076c4d6f986910227747e0b5a12b00a2fa
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-05 16:05:12 -0400

    fix: migrates

--- db/migrate/20240802203937_create_tasks.rb
+++ db/migrate/20240802203937_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.string :status
+      t.integer :status
       t.string :url
 
       t.timestamps
