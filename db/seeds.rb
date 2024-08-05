5569e3e0bbd301890af796e12084f947e4da2a19
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-05 16:07:43 -0400

    feat: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Limpar dados existentes (opcional)
+Task.destroy_all
+
+# Criar um usuário padrão (opcional)
+user = User.create(email: Faker::Internet.email)
+
+# Gerar dados de exemplo usando Faker e sample para enums
+20.times do
+  task = Task.create(
+    name: Faker::Lorem.sentence(word_count: 3),
+    status: Task.statuses.keys.sample,
+    url: Faker::Internet.url,
+    user_id: user.id,
+    description: Faker::Lorem.paragraph(sentence_count: 4),
+    task_type: Task.task_types.keys.sample
+  )
+
+end
+
+puts "Tasks geradas com sucesso!"
