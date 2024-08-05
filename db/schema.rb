f084abc8f117f84ce2ac4317d7e848082b56dbce
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-05 16:07:39 -0400

    feat: schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_02_203959) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_04_001806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.string "status"
+    t.integer "status"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.text "description"
+    t.integer "task_type"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
