b4160449e6625122769d14b91eee3b5276d0931d
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-07 14:48:06 -0400

    fix: name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tasks
 
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    name { Faker::Internet.username }
   end
 end
