325f304b14b9a09c7b4825fdc79032c67aa48602
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-07 14:49:16 -0400

    fix: notification

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -1,17 +1,32 @@
 class NotificationService
-  def self.send_notification(task, action)
+  attr_reader :task
+
+  def initialize(task)
+    @task = task
+  end
+
+  def send_notification
+    notification_data = {
+      notification: {
+        message: "Task #{@task.id} has been #{@task.persisted? ? 'updated' : 'created'}",
+        received_at: Time.current,
+        task_details: @task.as_json,
+        user_data: {
+          user_id: @task.user_id,
+          user_name: User.find(@task.user_id).name
+        }.as_json
+      }
+    }
+
     response = HTTParty.post("#{ENV['NOTIFICATION_SERVICE_URL']}/api/v1/notifications",
-                             body: {
-                               task_id: task.id,
-                               task_title: task.name,
-                               task_status: task.status,
-                               user_id: task.user_id,
-                               action: action
-                             }.to_json,
+                             body: notification_data.to_json,
                              headers: { 'Content-Type' => 'application/json' })
 
-    unless response.code == 201
+    if response.code == 201
+      true
+    else
       Rails.logger.error "Failed to send notification: #{response.body}"
+      false
     end
   end
 end
